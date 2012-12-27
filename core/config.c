69d0ed661b9a7c7de68071c3c4a251dd7b3768f3
Detegr <detegr@gmail.com>
2012-12-27 20:19:13 +0200

    Config work

--- core/config.c
+++ core/config.c
@@ -1,6 +1,183 @@
 #include "config.h"
+#include <stdlib.h>
+#include <string.h>
 
 static int item_compare(const void* a, const void* b)
 {
-	return strncmp(a,b,ITEM_MAXLEN);
+	return strncmp((const char*)a,((struct configitem*)b)->key,ITEM_MAXLEN);
+}
+
+static int section_compare(const void* a, const void* b)
+{
+	return strncmp((const char*)a,((struct configsection*)b)->name,ITEM_MAXLEN);
+}
+
+static int section_sort(const void* a, const void* b)
+{
+	return strncmp(((struct configsection*)a)->name,((struct configsection*)b)->name,ITEM_MAXLEN);
+}
+
+static void item_free(struct configitem* item)
+{
+	free(item->key);
+	if(item->val) free(item->val);
+}
+
+static void section_free(struct configsection* sect)
+{
+	free(sect->name);
+	for(unsigned int i=0; i<sect->items; ++i)
+	{
+		item_free(sect->item[i]);
+	}
+	free(sect->item);
+}
+
+void config_free(struct config* conf)
+{
+	if(conf->filename) free(conf->filename);
+	for(unsigned int i=0; i<conf->sections; ++i)
+	{
+		section_free(conf->section[i]);
+	}
+	free(conf->section);
+}
+
+static int item_find(const char* needle, struct configsection* haystack)
+{
+	int max=haystack->items;
+	int min=0;
+	while(max>=min)
+	{
+		int mid=(unsigned int)((max-min)/2);
+		int c = item_compare(needle, haystack->item[mid]);
+		if(c<0) min=mid+1;
+		else if(c>0) max=mid-1;
+		else return mid;
+	}
+	return -1;
+}
+
+void config_init(struct config* conf, const char* filename)
+{
+	conf->filename = strdup(filename);
+	conf->sections = 0;
+	conf->size = 0;
+	conf->section=NULL;
+}
+
+static void item_add(struct configsection* section, const char* key, const char* val)
+{
+	/* Allocate more space if needed */
+	if(section->items+1 > section->size)
+	{
+		struct configitem** newi = (struct configitem**)malloc(section->size?section->size*2:10 * sizeof(struct configitem*));
+		memcpy(newi, section->item, section->size);
+		section->size = section->size?section->size*2:10;
+		/* Free old item */
+		struct configitem** tmp=section->item;
+		section->item = newi;
+		free(tmp);
+	}
+	/* Assign new item */
+	struct configitem* newitem = (struct configitem*)malloc(sizeof(struct configitem));
+	newitem->key = strdup(key);
+	if(val) newitem->val = strdup(val);
+	else newitem->val = NULL;
+	section->item[section->items++] = newitem;
+}
+
+void config_add(struct config* conf, const char* section, const char* key, const char* val)
+{
+	struct configsection* sect;
+	if(conf->sections > 0 && (sect=config_find_section(section, conf)))
+	{
+		item_add(sect, key, val);
+	}
+	else
+	{
+		section_new(conf, section);
+		config_add(conf, section, key, val);
+	}
+}
+
+static void section_new(struct config* conf, const char* name)
+{
+	if(conf->sections+1 > conf->size)
+	{
+		struct configsection** news = (struct configsection**)malloc(conf->size?conf->size*2:10 * sizeof(struct configsection*));
+		memcpy(news, conf->section, conf->size);
+		conf->size = conf->size?conf->size*2:10;
+		/* Free old item */
+		struct configsection** tmp=conf->section;
+		conf->section = news;
+		free(tmp);
+	}
+	/* Assign new item */
+	struct configsection* newsection = (struct configsection*)malloc(sizeof(struct configsection));
+	newsection->name = strdup(name);
+	newsection->items = 0;
+	newsection->size = 0;
+	conf->section[conf->sections++] = newsection;
+
+	qsort(conf->section, conf->sections, sizeof(struct configsection*), section_sort);
+}
+
+struct configsection* config_find_section(const char* needle, struct config* haystack)
+{
+	int max=haystack->sections;
+	int min=0;
+	while(max>=min)
+	{
+		int mid=(unsigned int)((max-min)/2);
+		int c = section_compare(needle, haystack->section[mid]);
+		if(c<0) min=mid+1;
+		else if(c>0) max=mid-1;
+		else return haystack->section[mid];
+	}
+	return NULL;
+}
+
+struct configitem* config_find_item(const char* needle, struct config* haystack, const char* section)
+{
+	if(section)
+	{
+
+	}
+	else
+	{
+		for(unsigned int i=0; i<haystack->sections; ++i)
+		{
+			int item=item_find(needle, haystack->section[i]);
+			if(item != -1) return haystack->section[i]->item[item];
+		}
+	}
+	return NULL;
+}
+
+void config_flush(struct config* conf, FILE* stream)
+{
+	for(unsigned int i=0; i<conf->sections; ++i)
+	{
+		fprintf(stream, "[%s]\n", conf->section[i]->name);
+		for(unsigned int j=0; j<conf->section[i]->items; ++j)
+		{
+			struct configitem* item = conf->section[i]->item[i];
+			if(item->val)
+			{
+				fprintf(stream, "\t%s=%s\n", item->key, item->val);
+			}
+			else fprintf(stream, "\t%s\n", item->key);
+		}
+	}
+}
+
+int main()
+{
+	struct config c;
+	config_init(&c, "test.conf");
+	config_add(&c, "Section", "Key", "Value");
+	config_add(&c, "Aasi", "Don", "Key");
+	config_flush(&c, stdout);
+	return 0;
 }
--- core/config.h
+++ core/config.h
@@ -1,6 +1,7 @@
 #ifndef TAPI2P_CONFIG_H
 #define TAPI2P_CONFIG_H
 
+#include <stdio.h>
 #define ITEM_MAXLEN 255
 
 struct configitem
@@ -12,22 +13,36 @@ struct configitem
 struct configsection
 {
 	char* name;
-	struct configitem* item;
+	unsigned int items;
+	unsigned int size;
+	struct configitem** item;
 };
 
 struct config
 {
 	char* filename;
-	struct configitem* sections;
+	unsigned int sections;
+	unsigned int size;
+	struct configsection** section;
 };
 
 static int item_compare(const void* a, const void* b);
+static int section_compare(const void* a, const void* b);
+static int section_sort(const void* a, const void* b);
 
+static void item_add(struct configsection* section, const char* key, const char* val);
+static int item_find(const char* needle, struct configsection* haystack);
 static void item_free(struct configitem* item);
+
+static void section_new(struct config* conf, const char* name);
 static void section_free(struct configsection* sect);
 
+struct configsection* config_find_section(const char* needle, struct config* haystack);
+struct configitem* config_find_item(const char* needle, struct config* haystack, const char* section);
+void config_init(struct config* conf, const char* filename);
 void config_free(struct config* conf);
 void config_load(struct config* conf, const char* filename);
-void config_flush();
+void config_add(struct config* conf, const char* section, const char* key, const char* val);
+void config_flush(struct config* conf, FILE* stream);
 
 #endif
